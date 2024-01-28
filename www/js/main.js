a8fa7b2ed927a07fcc0ba53c36d2313e0b3fe2f8
ipmanlk <io@navinda.xyz>
2024-01-28 19:29:54 +0530

    fix: datamuse api

--- www/js/main.js
+++ www/js/main.js
@@ -18,21 +18,20 @@ ons.ready(() => {
 const onsenInit = () => {
   ons.disableDeviceBackButtonHandler();
   document.addEventListener("backbutton", onBackKeyDown, false);
-}
+};
 
 const onsenSlideBarInit = () => {
   window.fn = {};
   window.fn.open = () => {
-    let menu = document.getElementById('menu');
+    let menu = document.getElementById("menu");
     menu.open();
   };
   window.fn.load = (page) => {
-    let content = document.getElementById('content');
-    let menu = document.getElementById('menu');
-    content.load(page)
-      .then(menu.close.bind(menu));
+    let content = document.getElementById("content");
+    let menu = document.getElementById("menu");
+    content.load(page).then(menu.close.bind(menu));
   };
-}
+};
 
 // asign json data to global en2sn & sn2en objects
 const dbLoad = (data, db) => {
@@ -47,7 +46,7 @@ const dbLoad = (data, db) => {
   }
   // hide loading screen if sn2en is not null
   sn2en !== null ? modalLoadingHide() : null;
-}
+};
 
 // resolve file system urls and call jsonFileRead
 const dbRead = () => {
@@ -55,68 +54,81 @@ const dbRead = () => {
   const dbEn2Sn = cordova.file.applicationDirectory + "www/db/en2sn.json";
   const dbSn2En = cordova.file.applicationDirectory + "www/db/sn2en.json";
 
-  resolveLocalFileSystemURL(dbEn2Sn, (fileEntry) => {
-    jsonFileRead(fileEntry, "en2sn").then(data => {
-      dbLoad(data.json, data.db);
-    });
-  }, (error) => {
-    dbLoadError(error);
-    reject(error);
-  });
+  resolveLocalFileSystemURL(
+    dbEn2Sn,
+    (fileEntry) => {
+      jsonFileRead(fileEntry, "en2sn").then((data) => {
+        dbLoad(data.json, data.db);
+      });
+    },
+    (error) => {
+      dbLoadError(error);
+      reject(error);
+    }
+  );
 
-  resolveLocalFileSystemURL(dbSn2En, (fileEntry) => {
-    jsonFileRead(fileEntry, "sn2en").then(data => {
-      dbLoad(data.json, data.db);
-    });
-  }, (error) => {
-    dbLoadError(error);
-    reject(error);
-  });
-}
+  resolveLocalFileSystemURL(
+    dbSn2En,
+    (fileEntry) => {
+      jsonFileRead(fileEntry, "sn2en").then((data) => {
+        dbLoad(data.json, data.db);
+      });
+    },
+    (error) => {
+      dbLoadError(error);
+      reject(error);
+    }
+  );
+};
 
 // read .json files uisng file plugin
 const jsonFileRead = (fileEntry, db) => {
   return new Promise((resolve, reject) => {
-    fileEntry.file((file) => {
-      const reader = new FileReader();
-      reader.onloadend = function () {
-        resolve({ db, json: JSON.parse(this.result) })
-      };
-      reader.readAsText(file);
-    }, (error) => {
-      dbLoadError(error);
-      reject(error);
-    });
-  })
-}
+    fileEntry.file(
+      (file) => {
+        const reader = new FileReader();
+        reader.onloadend = function () {
+          resolve({ db, json: JSON.parse(this.result) });
+        };
+        reader.readAsText(file);
+      },
+      (error) => {
+        dbLoadError(error);
+        reject(error);
+      }
+    );
+  });
+};
 
 const dbLoadError = (error) => {
-  ons.notification.alert("Unable to load the database : " + e).then(function () {
-    exitApp();
-  });
-}
+  ons.notification
+    .alert("Unable to load the database : " + e)
+    .then(function () {
+      exitApp();
+    });
+};
 
 const modalLoadingShow = (txt) => {
-  const modal = $('#modalLoading');
+  const modal = $("#modalLoading");
   $("#modalLoadingMsg").text(" " + txt);
   modal.show();
-}
+};
 
 const modalLoadingHide = () => {
-  const modal = $('#modalLoading');
+  const modal = $("#modalLoading");
   modal.hide();
-}
+};
 
 const suggShow = (element) => {
-  $('#listSugg').empty();
+  $("#listSugg").empty();
   const input = inputClean($(element).val());
   if (!isNullOrEmpty(input)) wordSearch(input);
-}
+};
 
 //  use to search for words start with n
 const wordSearch = (input) => {
   let db;
-  langDetect(input) == "en2sn" ? db = en2snKeys : db = sn2enKeys;
+  langDetect(input) == "en2sn" ? (db = en2snKeys) : (db = sn2enKeys);
   // no of suggestions
   const limit = 10;
   let counter = 0;
@@ -131,9 +143,11 @@ const wordSearch = (input) => {
   if (counter == 0 && onlineCheck()) {
     // clean if there are any online suggestions
     $("#listSugg").empty();
-    $("#listSugg").append(`<ons-card onclick="gtranslateGet('${input}')"><div class="content"><h4>Sorry!. I couldn't find anything on that. Click here to get results from online.</h4></div></ons-card>`);
+    $("#listSugg").append(
+      `<ons-card onclick="gtranslateGet('${input}')"><div class="content"><h4>Sorry! I couldn't find anything on that. Click here to get results from online.</h4></div></ons-card>`
+    );
   }
-}
+};
 
 const meaningShow = (input) => {
   if (langDetect(input) == "en2sn") {
@@ -146,7 +160,7 @@ const meaningShow = (input) => {
 
   // save history
   historySave(input);
-}
+};
 
 const suggListAppend = (word, type) => {
   let listItem;
@@ -155,103 +169,124 @@ const suggListAppend = (word, type) => {
   } else {
     listItem = `<ons-card onclick="meaningShow('${word}')"><div class="content">${word}<span style="float: right"><ons-icon icon="md-translate"></ons-icon></span></div></ons-card>`;
   }
-  $('#listSugg').append(listItem);
-}
+  $("#listSugg").append(listItem);
+};
 
-const inputClean = (input) => ((input.trim()).toLowerCase())
+const inputClean = (input) => input.trim().toLowerCase();
 
-const isNullOrEmpty = (input) => jQuery.isEmptyObject(input)
+const isNullOrEmpty = (input) => jQuery.isEmptyObject(input);
 
 // check language
 const langDetect = (txt) => {
   let selectedDb;
-  const test = (/^[A-Za-z][\sA-Za-z0-9.\'-]*$/.test(txt));
-  test ? selectedDb = "en2sn" : selectedDb = "sn2en";
-  return (selectedDb);
-}
+  const test = /^[A-Za-z][\sA-Za-z0-9.\'-]*$/.test(txt);
+  test ? (selectedDb = "en2sn") : (selectedDb = "sn2en");
+  return selectedDb;
+};
 
 // tasks not releated to main.html template
 const snMeaningShow = (input) => {
-  const content = document.querySelector('ons-splitter-content');
-  content.load('./views/snMeaning.html').then(function () {
+  const content = document.querySelector("ons-splitter-content");
+  content.load("./views/snMeaning.html").then(function () {
     $("#title").text(input);
     const meanings = en2sn[input];
     for (let i = 0; i < meanings.length; i++) {
-      $('.listMeanings').append(`<ons-list-item>${meanings[i]}<div class="right"><ons-icon size="20px" onclick="textCopy(this);" icon="md-copy" class="list-item__icon"></ons-icon></div></ons-list-item>`);
+      $(".listMeanings").append(
+        `<ons-list-item>${meanings[i]}<div class="right"><ons-icon size="20px" onclick="textCopy(this);" icon="md-copy" class="list-item__icon"></ons-icon></div></ons-list-item>`
+      );
     }
     // call for datamuse
     if (onlineCheck()) {
-      datamuseGet(input).then(res => {
-        enDefShow(res.defs);
-        enSynShow(res.syns);
-      }).catch(error => {
-        console.log(error);
-      });
+      datamuseGet(input)
+        .then((res) => {
+          enDefShow(res.defs);
+          enSynShow(res.syns);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     } else {
-      $(".listSynonyms").append("<ons-list-item>You're offline. Please connect to the internet.</ons-list-item>");
-      $(".listDefinitions").append("<ons-list-item>You're offline. Please connect to the internet.</ons-list-item>");
+      $(".listSynonyms").append(
+        "<ons-list-item>You're offline. Please connect to the internet.</ons-list-item>"
+      );
+      $(".listDefinitions").append(
+        "<ons-list-item>You're offline. Please connect to the internet.</ons-list-item>"
+      );
     }
   });
-}
+};
 
 const enMeaningShow = (input) => {
-  const content = document.querySelector('ons-splitter-content');
-  content.load('./views/enMeaning.html').then(function () {
+  const content = document.querySelector("ons-splitter-content");
+  content.load("./views/enMeaning.html").then(function () {
     $("#title").text(input);
     const meanings = sn2en[input];
     for (let i = 0; i < meanings.length; i++) {
-      $('.listMeanings').append(`<ons-list-item>${meanings[i]}<div class="right"><ons-icon size="20px" onclick="textCopy(this);" icon="md-copy" class="list-item__icon"></ons-icon></div></ons-list-item>`);
+      $(".listMeanings").append(
+        `<ons-list-item>${meanings[i]}<div class="right"><ons-icon size="20px" onclick="textCopy(this);" icon="md-copy" class="list-item__icon"></ons-icon></div></ons-list-item>`
+      );
     }
     if (onlineCheck()) {
-      datamuseGet(meanings[0]).then(res => {
-        enDefShow(res.defs);
-      }).catch(error => {
-        console.log(error);
-      });
+      datamuseGet(meanings[0])
+        .then((res) => {
+          enDefShow(res.defs);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     } else {
-      $(".listDefinitions").append("<ons-list-item>You're offline. Please connect to the internet.</ons-list-item>");
+      $(".listDefinitions").append(
+        "<ons-list-item>You're offline. Please connect to the internet.</ons-list-item>"
+      );
     }
     snSynShow();
-  })
-}
+  });
+};
 
-const enDefShow = (data) => {
-  if (!isNullOrEmpty(data) && data.defs) {
-    const defs = data.defs;
-    let def; // definition
-    let pos; // part of speech
+const enDefShow = (defs) => {
+  if (defs.length > 0) {
     for (let i = 0; i < defs.length; i++) {
-      pos = defs[i].substr(0, defs[i].indexOf('	'));
+      let [pos, def] = defs[i].split("\t");
       pos = `(${datamuseExpandPos(pos)})`;
-      def = defs[i].substr(defs[i].indexOf(' ') + 1);
-      $(".listDefinitions").append(`<ons-list-item>${pos} ${def}</ons-list-item>`);
+
+      $(".listDefinitions").append(
+        `<ons-list-item>${pos} ${def}</ons-list-item>`
+      );
     }
   } else {
-    $(".listDefinitions").append("<ons-list-item>Sorry!. No results found.</ons-list-item>");
+    $(".listDefinitions").append(
+      "<ons-list-item>Sorry! No results found.</ons-list-item>"
+    );
   }
-}
+};
 
 const enSynShow = (syns) => {
   if (!isNullOrEmpty(syns)) {
     for (let i = 0; i < syns.length; i++) {
-      $(".listSynonyms").append(`<ons-list-item>${syns[i].word}<div class="right"><ons-icon onclick="TTSspeak('${syns[i].word}');" icon="ion-volume-high" class="list-item__icon"></ons-icon></div></ons-list-item>`);
+      $(".listSynonyms").append(
+        `<ons-list-item>${syns[i].word}<div class="right"><ons-icon onclick="TTSspeak('${syns[i].word}');" icon="ion-volume-high" class="list-item__icon"></ons-icon></div></ons-list-item>`
+      );
     }
   } else {
-    $(".listSynonyms").append("<ons-list-item>Sorry!. No results found.</ons-list-item>");
+    $(".listSynonyms").append(
+      "<ons-list-item>Sorry! No results found.</ons-list-item>"
+    );
   }
-}
+};
 
 const snSynShow = () => {
-  const enMeaning = (sn2en[selectedWord][0]).trim();
+  const enMeaning = sn2en[selectedWord][0].trim();
   const snSyns = en2sn[enMeaning];
   if (!isNullOrEmpty(snSyns)) {
     for (let i = 0; i < snSyns.length; i++) {
       $(".listSynonyms").append(`<ons-list-item>${snSyns[i]}</ons-list-item>`);
     }
   } else {
-    $(".listSynonyms").append("<ons-list-item>Sorry!. No results found.</ons-list-item>");
+    $(".listSynonyms").append(
+      "<ons-list-item>Sorry! No results found.</ons-list-item>"
+    );
   }
-}
+};
 
 // expand part of speech
 const datamuseExpandPos = (pos) => {
@@ -273,8 +308,8 @@ const datamuseExpandPos = (pos) => {
       expandedPos = "unknown";
       break;
   }
-  return (expandedPos);
-}
+  return expandedPos;
+};
 
 // text to speach plugin
 const TTSspeak = (input) => {
@@ -286,19 +321,21 @@ const textCopy = (element) => {
   const text = $(element).parent().parent().text();
   cordova.plugins.clipboard.copy(text);
   toastShow("Text copied to clipboard!");
-}
+};
 
-const toastShow = (msg) => ons.notification.toast(msg, { timeout: 1000, animation: 'ascend' })
+const toastShow = (msg) =>
+  ons.notification.toast(msg, { timeout: 1000, animation: "ascend" });
 
 // about
-const aboutShow = () => $('#modalAbout').show()
+const aboutShow = () => $("#modalAbout").show();
 
 // online check
 const onlineCheck = () => {
   const networkState = navigator.connection.type;
-  const isOffline = (networkState == Connection.NONE) || (networkState == Connection.UNKNOWN);
+  const isOffline =
+    networkState == Connection.NONE || networkState == Connection.UNKNOWN;
   return !isOffline;
-}
+};
 
 const tabSwitch = (id) => {
   $("#listMeanings").parent().hide();
@@ -306,4 +343,4 @@ const tabSwitch = (id) => {
   $("#listSynonyms").parent().hide();
 
   $(id).parent().fadeIn();
-}
+};
--- www/js/requests.js
+++ www/js/requests.js
@@ -1,64 +1,83 @@
 // datamuse api calls
-const datamuseGet = (word) => {
-    return new Promise((resolve, reject) => {
-        const url = "https://osd-online.appspot.com/datamuse";
-        request(url, "get", { word }).then(res => {
-            resolve(res);
-        }).catch(error => {
-            console.log(error);
-            reject(error);
-        })
-    });
-}
+const datamuseGet = async (word) => {
+	let defs = [];
+	let syns = [];
 
-// google translate api calls
+	// get definitions
+	await request("https://api.datamuse.com/words", "get", {
+		sp: word,
+		md: "dp",
+	})
+		.then((data) => {
+			if (data.length > 0) {
+				defs = data[0].defs || [];
+			}
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+
+	// get synonyms
+	await request("https://api.datamuse.com/words", "get", {
+		rel_syn: word,
+	}).then((data) => {
+		if (data.length > 0) {
+			syns = data;
+		}
+	});
+	return { defs, syns };
+};
+
 const gtranslateGet = (input) => {
-    modalLoadingShow("Getting results...");
-    $("#listSugg").empty();
+	modalLoadingShow("Getting results...");
+	$("#listSugg").empty();
 
-    request("https://osd-online.appspot.com/translate", "get", { word: input }).then((data) => {
-        if (!data.error) {
-            // add meaning to global objects
-            if (langDetect(input) == "en2sn") {
-                en2sn[input] = data;
-                en2snKeys.push(input);
-                en2snKeys.sort();
-            } else {
-                sn2en[input] = data;
-                sn2enKeys.push(input);
-                sn2enKeys.sort();
-            }
-            suggListAppend(input, "online");
-            modalLoadingHide();
-        } else {
-            modalLoadingHide();
-            ons.notification.alert("Your input is incorrect!. Please check for spelling mistakes.");
-        }
-    }).catch(res => {
-        console.log(res);
-    })
-
-}
+	request("https://osd-online.appspot.com/translate", "get", { word: input })
+		.then((data) => {
+			if (!data.error) {
+				// add meaning to global objects
+				if (langDetect(input) == "en2sn") {
+					en2sn[input] = data;
+					en2snKeys.push(input);
+					en2snKeys.sort();
+				} else {
+					sn2en[input] = data;
+					sn2enKeys.push(input);
+					sn2enKeys.sort();
+				}
+				suggListAppend(input, "online");
+				modalLoadingHide();
+			} else {
+				modalLoadingHide();
+				ons.notification.alert(
+					"Your input is incorrect!. Please check for spelling mistakes."
+				);
+			}
+		})
+		.catch((res) => {
+			console.log(res);
+		});
+};
 
 const request = (url, type, data) => {
-    return new Promise((resolve, reject) => {
-        $("#progressBar").fadeIn();
-        $.ajax({
-            url: url,
-            type: type,
-            data: data,
-            timeout: 30000,
-            dataType: "json",
-            success: function (res) {
-                $("#progressBar").fadeOut();
-                resolve(res);
-            },
-            error: function (error) {
-                toastShow("Request failed!");
-                modalLoadingHide();
-                $("#progressBar").fadeOut();
-                reject(error);
-            }
-        });
-    })
-}
+	return new Promise((resolve, reject) => {
+		$("#progressBar").fadeIn();
+		$.ajax({
+			url: url,
+			type: type,
+			data: data,
+			timeout: 30000,
+			dataType: "json",
+			success: function (res) {
+				$("#progressBar").fadeOut();
+				resolve(res);
+			},
+			error: function (error) {
+				toastShow("Request failed!");
+				modalLoadingHide();
+				$("#progressBar").fadeOut();
+				reject(error);
+			},
+		});
+	});
+};
